239070423f4162bb425154b9d2893c87d06a1da4
Deborah Stuart <Debs@unknown.home>
2021-03-25 18:15:17 +0000

    Connection to db and displaying simple relational content on page

--- create_db/create_database.py
+++ create_db/create_database.py
@@ -6,16 +6,23 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
  
 Base = declarative_base()
- 
+
 class Article(Base):
     __tablename__ = 'article'
     id = Column(Integer, primary_key=True)
     title = Column(String)
     date = Column(DATE)
  
+class Song(Base):
+    __tablename__ = 'song'
+    id = Column(Integer, primary_key=True)
+    artist = Column(String)
+    name = Column(String)
+    article_no = Column(Integer,ForeignKey('article.id'))
+ 
 # Create an engine that stores data in the local directory's
 # sqlalchemy_example.db file.
-engine = create_engine('sqlite:///../data/SSdb.db')
+engine = create_engine('sqlite:///../data/SSdb.sqlite')
  
 # Create all tables in the engine. This is equivalent to "Create Table"
 # statements in raw SQL.
